e5ccdf6b1fc6ba4052655f8fc4c951673db3d0f6
786mubin <52349577+786mubin@users.noreply.github.com>
2021-03-02 00:10:38 +0530

    Update Digital_clock_GUI.py

--- Digital_clock_GUI.py
+++ Digital_clock_GUI.py
@@ -8,12 +8,11 @@ root.title("Digital Clock")
 
 def times():
     obj=datetime.datetime.now()
-    time=obj.strftime('%H : %M : %S : %p')
+    time=obj.strftime("%r")
     label.config(text=time)
     label.after(1000, times)
     return time
 
-# Label(root, text="Welcome to the digital clock").pack()
 label=Label(root,font=("ds-digital", 80), background="black", foreground="cyan" )
 label.pack(anchor="center")
 times()
